9ae1fdcfd7d8ff82656af7af5cebadb0b2630f6b
Shenol Osman <shenol.osman@utb.ecutbildning.se>
2021-09-15 14:43:17 +0200

    [Red/Green] TestCantRegisterSameUserTwice

--- Core/LoginManager.cs
+++ Core/LoginManager.cs
@@ -4,9 +4,27 @@ namespace Core
 {
     public class LoginManager
     {
+        private string _username;
+        private string _password;
         public void RegisterNewUser(string username, string password)
         {
+            if (_username!=username)
+            {
+                _username = username;
+                _password = password;
+            }
+        }
 
+        public bool CheckSameUsers(string username, string password)
+        {
+            if (_username==username && _password==password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Tests/LoginTestSuite.cs
+++ Tests/LoginTestSuite.cs
@@ -17,8 +17,20 @@ namespace Tests
             loginManager.RegisterNewUser("user", "pass");
 
             //Assert
+            Assert.True(loginManager.CheckSameUsers("user","pass"));
 
+        }
 
+        [Fact]
+        private void TestCantRegisterSameUserTwice()
+        {
+            LoginManager loginManager = new();
+
+            loginManager.RegisterNewUser("user","pwd");
+            loginManager.RegisterNewUser("user","pwd_diff");
+
+           Assert.True(loginManager.CheckSameUsers("user", "pwd"));
+           Assert.False(loginManager.CheckSameUsers("user", "pwd_diff"));
         }
     }
 }
